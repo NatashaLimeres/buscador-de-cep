e61a0d3e76eb751f18106cf1af218ae78e306bf8
Natasha Limeres <natashaslimeres@gmail.com>
2022-10-25 22:49:23 -0300

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch(){
+    //01310930/json
+    if(input === ''){
+      alert ('Preencha algum CEP')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    } catch {
+      alert('Ops! Erro ao buscar')
+      setInput('')
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="title">BUSCADOR CEP</h1>
@@ -10,20 +33,26 @@ function App() {
         <input 
         type="text"
         placeholder="Digite seu CEP..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
         />
 
-        <button className="searchButton">
+        <button className="searchButton" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
-      
+
+
+
+      {Object.keys(cep).length > 0 && (
       <main className="main">
-        <h2>CEP: 11070060</h2>
-        <span>Rua doutor moura ribeiro</span>
-        <span>Complemento: 108 </span>
-        <span>Marapé</span>
-        <span>Santos - SP</span>
-      </main>
+      <h2>CEP: {cep.cep}</h2>
+      <span>{cep.logradouro}</span>
+      <span>Complemento: {cep.complemento}</span>
+      <span>{cep.bairro}</span>
+      <span>{cep.localidade} - {cep.uf}</span>
+    </main>
+      )}
 
     </div>
   );
